611f74d277b2119f3d21d1e61d8f886bbce89476
Rodolphe Stoclin <srodolphe@gmail.com>
2023-05-29 22:58:39 +0200

    Update

--- scripts/installs/set_ssh_key.sh
+++ scripts/installs/set_ssh_key.sh
@@ -8,9 +8,9 @@ ssh-keygen -t ed25519 -C $email -f ~/.ssh/id_ed25519
 eval "$(ssh-agent -s)"
 touch ~/.ssh/config
 if [ "$(uname -s)" = "Darwin" ]; then
-  echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+  echo "Host *\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
   ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 else
-  echo "Host *\n AddKeysToAgent yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+  echo "Host *\n  AddKeysToAgent yes\n  IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
   ssh-add ~/.ssh/id_ed25519
 fi
